9878d807ed82976c3f9fabfa61ac205cdbc3debd
turan <liaojian.2008.ok@163.com>
2019-05-29 14:05:23 +0800

    test code

--- domain-test/src/main/java/com/example/Application.java
+++ domain-test/src/main/java/com/example/Application.java
@@ -1,12 +1,7 @@
 package com.example;
 
-import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @Description
@@ -17,23 +12,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-    @Value("${http.port}")
-    private Integer port;
-
     public static void main(String args[]) {
         SpringApplication.run(Application.class);
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(port);
-        return connector;
-    }
 }
